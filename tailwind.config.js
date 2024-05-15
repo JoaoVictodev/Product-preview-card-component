ccceed6ba2366aafade7fbf2f1f42ecfc6b803ce
João Victor <themaciel190@gmail.com>
2024-05-15 18:44:28 -0500

    Gabrielle Essence Eau De Parfum desktop

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        "a-main": "28.125rem",
+        "l-main": "37.5rem",
+      },
+      colors: {
+        "Dark-cyan": "hsl(158, 36%, 37%)",
+        Cream: "#f2ebe3",
+        "Very-dark-blue": "hsl(212, 21%, 14%)",
+        "Dark-grayish-blue": "hsl(228, 12%, 48%)",
+        "White-main": "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat, sans-serif"],
+        fraunces: ["Fraunces, sans-serif"],
+      },
+      letterSpacing: {
+        widest: "0.188rem",
+        "tracking-tighter": "3px",
+      },
+      backgroundImage: ["after"],
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".bg-img-after": {
+            content: '""',
+            position: "absolute",
+            top: "17px",
+            left: "45px",
+            width: "15px",
+            height: "16px",
+            "background-image": "url('../images/icon-cart.svg')",
+            "background-size": "cover",
+            "background-position": "center",
+          },
+        },
+        ["after"]
+      );
+    },
+  ],
+};
